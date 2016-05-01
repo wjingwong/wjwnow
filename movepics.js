85058dec70edf2c029afa2cc0af68a9b0143db89
wjingwong <wjw559@aol.com>
2016-05-01 09:34:37 -0700

    Update movepics.js

--- movepics.js
+++ movepics.js
@@ -9,18 +9,18 @@ function cycleItems() {
 }
 
 var autoSlide = setInterval(function() {
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
+  currentIndex += 5;
+  if (currentIndex > itemAmt - 5) {
+    currentIndex = 5;
   }
   cycleItems();
 }, 3000);
 
 $('.next').click(function() {
   clearInterval(autoSlide);
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
+  currentIndex += 5;
+  if (currentIndex > itemAmt - 5) {
+    currentIndex = 5;
   }
   cycleItems();
 });
