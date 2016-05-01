e335214c862a4c92910f5ae2771931815bfe063b
wjingwong <wjw559@aol.com>
2016-05-01 08:28:13 -0700

    Update movepics.js

--- movepics.js
+++ movepics.js
@@ -1,36 +1,35 @@
-$(".scrollable").scrollable();
- 
-$(".items img").click(function() {
-	// see if same thumb is being clicked
-	if ($(this).hasClass("active")) { return; }
- 
-	// calclulate large image's URL based on the thumbnail URL (flickr specific)
-	var url = $(this).attr("src").replace("_t", "");
- 
-	// get handle to element that wraps the image and make it semi-transparent
-	var wrap = $("#image_wrap").fadeTo("medium", 0.5);
- 
-	// the large image from www.flickr.com
-	var img = new Image();
- 
+var currentIndex = 0,
+  items = $('.container div'),
+  itemAmt = items.length;
 
-	// call this function after it's loaded
-	img.onload = function() {
- 
-		// make wrapper fully visible
-		wrap.fadeTo("fast", 1);
- 
-		// change the image
-		wrap.find("img").attr("src", url);
- 
-	};
- 
-	// begin loading the image from www.flickr.com
-	img.src = url;
- 
-	// activate item
-	$(".items img").removeClass("active");
-	$(this).addClass("active");
- 
-// when page loads simulate a "click" on the first image
-}).filter(":first").click();
+function cycleItems() {
+  var item = $('.container div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
+
+$('.next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('.prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});
